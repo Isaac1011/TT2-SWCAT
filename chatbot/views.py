4517568cadb2c050d6bc21fb707a0f7513fd81a5
Isaac Sandoval Acevedo <sandoval.5im6.v4@gmail.com>
2023-11-06 22:14:41 -0600

    Dando respuestas específicas, hay que mejorar

--- chatbot/views.py
+++ chatbot/views.py
@@ -3,14 +3,25 @@ from django.shortcuts import render
 import openai
 import spacy
 
+
 # Cargar el modelo en español de spaCy
 nlp = spacy.load("es_core_news_sm")
 
 # Diccionario de preguntas y respuestas
 faq = {
-    "¿Cuál es el objetivo de las tutorías?": "El objetivo de las tutorías es proporcionar apoyo académico personalizado para ayudarte a comprender mejor los temas y mejorar tu rendimiento académico.",
+    "¿Cuál es el objetivo de las tutorías?": " Las tutorías en el IPN tienen el objetivo de fortalecer el aprendizaje para convertirlo en una experiencia significativa, apoyar la formación integral del alumno y ser una guía para alcanzar metas escolares de manera exitosa. La acción tutorial tiene las siguientes acciones: 1. Asesorar académicamente en temas difíciles. 2. Apoyar a elegir la trayectoria académica ideal para los alumnos. 3. Fomentar el desarrollo de capacidades, habilidades, valores y actitudes. 4. Guiar a los alumnos para encontrar las estrategias que permitan lograr aprendizajes significativos. 5. Promover la autonomía para mejorar el desarrollo personal y el desempeño académico. ",
 
-    "¿Cómo inscribirme a una tutoría?": "Para inscribirte a una tutoría, debes seguir estos pasos: [Pasos detallados].",
+    "¿Cuáles son las áreas de intervención de las tutorías?": "1. Sentido de pertenencia. Para que el alumno se reconozca con orgullo como parte del IPN. 2. Acompañamiento durante la trayectoria escolar. Con el fin de evitar de evitar el riesgo de rezago, reprobación o abandono. 3. Orientación sobre servicios trámites y apoyos económicos. Desde becas institucionales hasta guiar al alumno a actividades deportivas para su desarrollo integral 4. Atención especializada y canalización. Se tratan otros factores que afectan el rendimiento escolar que pueden orillar al alumno a abandonar sus estudios, como problemas familiares, económicos o de salud. Los tutores están preparados para identificar problemáticas del estudiante y dirigirlo a las instancias adecuadas para que reciba atención y ayuda especializada.",
+
+    "¿Cómo inscribirme a una tutoría grupal?": "Para inscribirte a una tutoría grupal en SWCAT, debes seguir estos pasos: 1. Iniciar Sesión como Tutorado en SWCAT 2. Dar clic en 'Inscribirse a Tutoría Grupal' 3. Seleccionar la Tutoría Grupal de tu interés. Si ya estás inscrito en 3 tutorías no podrás incribirte 4. Listo, ya estás inscrito",
+
+    "¿Cómo inscribirme a una tutoría individual?": "Para inscribirte a una tutoría individual en SWCAT debes ponerte en contacto con la Tutora o Tutor que deseas tomar la tutoría, tu Tutora o Tutor se encargará de crear la Tutoría Individual, solo necesitas tener una cuenta en SWCAT",
+
+    "¿Cómo crear a una tutoría individual?": "Para inscribirte a una tutoría grupal en SWCAT, debes seguir estos pasos: 1. Iniciar Sesión como Tutor en SWCAT 2. Dar clic en 'Crear Tutoría Individual' 3. Seleccionar el Tutorado que desea tomar la tutoría, este debe una cuenta en SWCAT. Si ya estás inscrito en 3 tutorías no podrás incribirte 4. Darle un nombre a la tutoría individual 5.- Listo, se ha creado la tutoría individual",
+
+    "¿Cómo crear a una tutoría grupal?": "Para inscribirte a una tutoría grupal en SWCAT, debes seguir estos pasos: 1. Iniciar Sesión como Tutor en SWCAT 2. Dar clic en 'Crear Tutoría Grupal' 3. Darle un nombre a la tutoría grupal 4. Definir el salón donde se impartirá la tutoría 5. Listo, se ha creado la tutoría grupal",
+
+    "¿Qué es el Programa Institucional de Tutorías (PIT)?": "El Programa Institucional de Tutorías (PIT) es una estrategia que tiene como propósito organizar la tutoría en todas las Unidades Académicas del IPN para acompañar a los alumnos en temas personales y académicos, a lo largo de su trayectoria escolar. El PIT es una estrategia colaborativa para brindar acompañamiento personalizado a los estudiantes de nivel medio superior y superior durante su trayectoria escolar"
 }
 
 # Función para encontrar la pregunta más similar en el diccionario de FAQ
@@ -27,7 +38,10 @@ def get_most_similar_question(user_question, faq):
             max_similarity = similarity
             best_match = question
 
-    if max_similarity < 0.7:
+    print('Similitud')
+    print(max_similarity)
+
+    if max_similarity < 0.6:
         best_match = None
 
     return best_match
@@ -58,5 +72,5 @@ def chatbot(request):
         user_input = None
         bot_response = None
 
-    return render(request, 'chatbot.html', {'bot_response': bot_response,
-                                            'user_input': user_input})
+    return render(request, 'chatbot.html', {'user_input': user_input,
+                                            'bot_response': bot_response})
